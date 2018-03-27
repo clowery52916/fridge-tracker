817b31659a3322742e954c9792beacdfaa1e83d2
clowery <clowery52916@gmail.com>
2018-03-26 20:35:41 -0400

    messed up my delete and show single fridge, now fixed, commented out code is what I had before I had my git repo

--- lib/fridge_tracker.rb
+++ lib/fridge_tracker.rb
@@ -12,9 +12,8 @@ def run_tracker
   user_descision = gets.chomp.to_i
   if user_descision == 1
     p Fridge.all
-    puts 'What fridge do you want to look at? Please enter the id'
-    pp checkout_fridge(fridge_id) == gets.chomp.to_i
-  end
+
+    end
   if user_descision == 2
     puts 'Brand name of Fridge: '
     brand_input = gets.chomp.to_s
@@ -30,59 +29,97 @@ def run_tracker
  Fridge.create(brand: brand_input, size: size_input, location: location_input, has_food_in_it: food_input, has_drink_in_it: drink_input)
   end
   if user_descision == 3
-    puts 'Please enter the name of the fridge you would like to remove'
-    fridge_id = gets.chomp.to_s
-    Fridge.find(fridge_id)
-    fridge_id.destroy
+    puts 'Please enter the id of the fridge you would like to remove'
+    fridge_id = gets.chomp.to_i
+    # Fridge.find(fridge_id)
+    Fridge.find(fridge_id).destroy
+
   end
+
 end
-
-
 
 def checkout_fridge(fridge_id)
   id = fridge_id
-  if user_descision == 1
-    puts 'Food in fridge: '
-  end
-  if food_in_fridge = Food.all
-    p food_in_fridge
-   # p Food.find_by(name:'Pasta', name: 'Fruit', name:'Pizza')
+  puts 'What fridge do you want to look at? Please enter the id'
+  fridge_id == gets.chomps.to_i
+  Fridge.find(fridge_id)
+  fridge_id(id)
 end
 
-  # Find and Print a Fridge
-  puts <<~HEREDOC
-         What would you like to do with the fridge?
-         1. Checkout the Food
-         2. Checkout the Drinks
-         3. Return to fridge Menu
-         select a number:
-       HEREDOC
-  user_descision = gets.chomp.to_i
-  # Your code
-end
-def run_food_menu(fridge)
-  fridge = fridge
-  puts <<~HEREDOC
-         1. View All Food Items in a specific Firdge
-         2. Add a food item to a fridge
-         3. Eat a food item from a fridge
-         4. Go Back to Fridge Menu
-         Select a number:
-       HEREDOC
-  user_descision = gets.chomp.to_i
-  # Your Code Here
-end
-def run_drink_menu(fridge)
-  fridge = fridge
-  puts <<~HEREDOC
-         1. View all drink items in a specific fridge
-         2. Add a drink item to a fridge
-         3. Consume PART of a drink from a fridge
-         4. Consume ALL of a drink from a fridge
-         5. Go Back to Fridge Menu
-         Select a number:
-       HEREDOC
-  user_descision = gets.chomp.to_i
-  # Your Code Here
-end
+#
+#     end
+#   # if user_descision == 1
+#   #   puts 'Food in fridge: '
+#
+# #   if food_in_fridge = Food.all
+# #     p food_in_fridge
+# #    # p Food.find_by(name:'Pasta', name: 'Fruit', name:'Pizza')
+# # end
+#
+#   # Find and Print a Fridge
+#   puts <<~HEREDOC
+#          What would you like to do with the fridge?
+#          1. Checkout the Food
+#          2. Checkout the Drinks
+#          3. Return to fridge Menu
+#          select a number:
+#        HEREDOC
+#   user_descision = gets.chomp.to_i
+#   if user_descision == 1
+#           puts "View the food in the fridge"
+#          checkout_fridge = Food.ids
+#          puts 'Select and id of the fridge'
+#          pp checkout_fridge
+#         elsif
+#           user_descision == 2
+#           puts "View the drinks in the fridge"
+#           run_drink_menu(id)
+#         elsif
+#           user_descision == 3
+#           puts Fridge.ids
+#        puts "What fridge do you want to look at?"
+#        fridge_id = gets.chomp.to_s
+#        puts checkout_fridge(fridgeid)
+#         end
+#       end
+# def run_food_menu(fridge)
+#   fridge = fridge
+#   puts <<~HEREDOC
+#          1. View All Food Items in a specific Firdge
+#          2. Add a food item to a fridge
+#          3. Eat a food item from a fridge
+#          4. Go Back to Fridge Menu
+#          Select a number:
+#        HEREDOC
+#   user_descision = gets.chomp.to_i
+# #   if user_descision == 1
+# #           puts "Food in fridge"
+# #           run_food_menu(food_id)
+# #         elsif
+# #           user_descision == 2
+# #           puts "checkout the drinks"
+# #           run_drink_menu(id)
+# #         elsif
+# #           user_descision == 3
+# #           puts Fridge.all
+# #        puts "Enter the fridge Id"
+# #        fridge_id = gets.chomp.to_i
+# #        puts checkout_fridge(fridgeid)
+# #         end
+# # end
+# end
+# def run_drink_menu(fridge)
+#   fridge = fridge
+#   puts <<~HEREDOC
+#          1. View all drink items in a specific fridge
+#          2. Add a drink item to a fridge
+#          3. Consume PART of a drink from a fridge
+#          4. Consume ALL of a drink from a fridge
+#          5. Go Back to Fridge Menu
+#          Select a number:
+#        HEREDOC
+#   user_descision = gets.chomp.to_i
+#   # Your Code Here
+# end
+
 run_tracker
